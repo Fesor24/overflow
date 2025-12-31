74415eca8e9384e9538007f13e4f612361f0eed1
Wale <onafesowale@gmail.com>
2025-12-31 03:30:57 +0000

    postgres aspire integration

--- Overflow.AppHost/AppHost.cs
+++ Overflow.AppHost/AppHost.cs
@@ -7,8 +7,16 @@ var keycloak = builder.AddKeycloak("keycloak", 6001)
     .WithDataVolume("keycloak-data")
     .WithoutHttpsCertificate();
 
+var postgres = builder.AddPostgres("postgres", port: 5422)
+    .WithDataVolume("postgres-data")
+    .WithPgAdmin();
+
+var questionDb = postgres.AddDatabase("question-db");
+
 var questionService = builder.AddProject<Projects.QuestionService>("question-svc")
     .WithReference(keycloak)
-    .WaitFor(keycloak);
+    .WithReference(questionDb)
+    .WaitFor(keycloak)
+    .WaitFor(postgres);
 
 builder.Build().Run();
