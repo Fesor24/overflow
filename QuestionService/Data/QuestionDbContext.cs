6b5858875f8b104346dd3f581b5d141b20d1b12f
Wale <onafesowale@gmail.com>
2025-12-31 03:42:29 +0000

    entity classes

---
+++ QuestionService/Data/QuestionDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuestionService.Entities;
+
+namespace QuestionService.Data;
+
+public class QuestionDbContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<Question> Questions => Set<Question>();
+    public DbSet<Tag> Tags => Set<Tag>();
+}
